004da53a76b881751a1aa432e3fa6f3dea4bbebf
LonewolfLegion <59158102+LonewolfLegion@users.noreply.github.com>
2020-03-28 20:24:40 +0530

    Removing the project content

--- HackCovidAPI/Models/BusinessType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HackCovidAPI.Models
-{
-	public class BusinessType
-	{
-		public object _id { get; set; }
-		public int TypeId { get; set; }
-		public string Description { get; set; }
-	}
-}
